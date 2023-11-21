4405d23f85dd78b0661d2be3c98815fbfa045d2e
MarioBrbot <mariobrbot@gmail.com>
2023-11-21 17:49:57 +0100

    Added Interfaces and Search-Trains Styling

--- EuroTrains/ClientApp/src/app/search-trains/search-trains.component.ts
+++ EuroTrains/ClientApp/src/app/search-trains/search-trains.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchTrainsComponent implements OnInit {
 
+  searchResult: TrainsRm[] = [
+    {
+      company: "Hrvatske Željeznice",
+      remainingNumberOfSeats: 400,
+      departure: { time: Date.now().toString(), place: "Zagreb" },
+      arrival: { time: Date.now().toString(), place: "Osijek" },
+      price: "40",
+    },
+    {
+      company: "Hekurudha Shqiptare",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "Tirana" },
+      arrival: { time: Date.now().toString(), place: "Zagreb" },
+      price: "60",
+    },
+    {
+      company: "Österreichische Bundesbahnen",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "Vienna" },
+      arrival: { time: Date.now().toString(), place: "Zagreb" },
+      price: "60",
+    },
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+
+export interface TrainsRm {
+  company: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
